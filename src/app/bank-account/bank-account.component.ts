4d0891b876489ba9111799ee831b8b1e441d11fa
Giovanni Frate <giovanni.frate@studenti.unipegaso.it>
2025-04-11 21:13:33 +0200

    fix(bank-account): refresh sidenav configuration on navigation end

--- src/app/bank-account/bank-account.component.ts
+++ src/app/bank-account/bank-account.component.ts
@@ -1,8 +1,10 @@
-import { Component, computed, effect, inject } from '@angular/core';
-import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
+import { Component, effect, inject } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 
 import { BankAccountService } from './bank-account.service';
 import { SidenavService } from '../shared/services/sidenav.service';
+import { filter } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-bank-account',
@@ -11,14 +13,15 @@ import { SidenavService } from '../shared/services/sidenav.service';
   styleUrl: './bank-account.component.css'
 })
 export class BankAccountComponent {
-  private route = inject(ActivatedRoute);
   private router = inject(Router);
   protected bankAccountService = inject(BankAccountService);
   protected sidenavService = inject(SidenavService);
 
   protected bankAccountId = this.bankAccountService.currentBankAccountId;
+  protected routeChange = toSignal(this.router.events.pipe(filter((event) => event instanceof NavigationEnd)));
   protected changeNavlistOnBankAccountIdChange = effect(() => {
     const bankAccountId = this.bankAccountId();
+    this.routeChange();
     const navlistRoutes: { routePath: string; routeLabel: string; iconName?: string }[] = [
       {
         routePath: `/bank-account/${bankAccountId}/transaction`,
